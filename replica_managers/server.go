f81ca37966f50645c9eac8c55b885144915059e9
T.H. Engberg <tore0402@gmail.com>
2025-11-21 08:31:10 +0100

    auto generate protoc code and set up server listener

--- replica_managers/server.go
+++ replica_managers/server.go
@@ -1,3 +1,42 @@
 package main
 
-func main() {}
+import (
+	proto "ReplicationService/grpc"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+type ReplicationServiceServer struct {
+	proto.UnimplementedReplicationServiceServer
+}
+
+func main() {
+	addr := ":50051"
+
+	//Create TCP listener on specified port
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Server STARTUP_ERROR: failed to listen on %s: %v", addr, err)
+	}
+	//Creates server instance
+	grpcServer := grpc.NewServer()
+	//Register our service implementation with the gRPC server
+	proto.RegisterReplicationServiceServer(grpcServer, &ReplicationServiceServer{})
+
+	log.Printf("Server STARTUP: listening on %s", addr)
+
+	// run server
+	go func() {
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("Server ERROR: %v", err)
+		}
+	}()
+	//Block main goroutine -keep server running :)
+	select {}
+}
+
+// replicateToManagers(){}
+
+// handleReplication
